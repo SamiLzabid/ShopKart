20ef95f0f7951eb1a3753bef6a9bcdf126b36280
SamiLzabid <samizabid2000@gmail.com>
2026-04-05 19:55:46 +0600

    Update settings.py

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -135,9 +135,3 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'samizabid2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'uafu uthy wppk fnif'
-EMAIL_USE_TLS = True
